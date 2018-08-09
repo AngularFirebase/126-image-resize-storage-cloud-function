9ea55c2e19aba2ac27d7de56af7161a8db13e26f
codediodeio <hello@codediode.io>
2018-08-09 07:32:29 -0700

    full working function

---
+++ functions/src/index.ts
@@ -0,0 +1,59 @@
+import * as functions from 'firebase-functions';
+
+import * as Storage from '@google-cloud/storage';
+const gcs = Storage();
+
+import { tmpdir } from 'os';
+import { join, dirname } from 'path';
+
+import * as sharp from 'sharp';
+import * as fs from 'fs-extra';
+
+export const generateThumbs = functions.storage
+  .object()
+  .onFinalize(async object => {
+    const bucket = gcs.bucket(object.bucket);
+    const filePath = object.name;
+    const fileName = filePath.split('/').pop();
+    const bucketDir = dirname(filePath);
+
+    const workingDir = join(tmpdir(), 'thumbs');
+    const tmpFilePath = join(workingDir, 'source.png');
+
+    if (fileName.includes('thumb@') || !object.contentType.includes('image')) {
+      console.log('exiting function');
+      return false;
+    }
+
+    // 1. Ensure thumbnail dir exists
+    await fs.ensureDir(workingDir);
+
+    // 2. Download Source File
+    await bucket.file(filePath).download({
+      destination: tmpFilePath
+    });
+
+    // 3. Resize the images and define an array of upload promises
+    const sizes = [64, 128, 256];
+
+    const uploadPromises = sizes.map(async size => {
+      const thumbName = `thumb@${size}_${fileName}`;
+      const thumbPath = join(workingDir, thumbName);
+
+      // Resize source image
+      await sharp(tmpFilePath)
+        .resize(size, size)
+        .toFile(thumbPath);
+
+      // Upload to GCS
+      return bucket.upload(thumbPath, {
+        destination: join(bucketDir, thumbName)
+      });
+    });
+
+    // 4. Run the upload operations
+    await Promise.all(uploadPromises);
+
+    // 5. Cleanup remove the tmp/thumbs from the filesystem
+    return fs.remove(workingDir);
+  });
